f623a7da6dc4e0cd3b071be58a260e1cce357300
Ajdin <ajdintotti5@gmail.com>
2020-10-12 03:01:42 +0200

    created bookmodel and bookcontroller

--- BookStore/BookStore/Controllers/BookController.cs
+++ BookStore/BookStore/Controllers/BookController.cs
@@ -2,15 +2,33 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BookStore.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BookStore.Controllers
 {
     public class BookController : Controller
     {
-        public string GetAllBooks()
+        private readonly BookModel _bookModel = null;
+        public BookController()
         {
-            return "All books";
+            _bookModel = new BookModel();
         }
+
+        public List<BookModel> GetAllBooks()
+        {
+            return _bookModel.GetAllBooks();  
+        }
+
+        public BookModel GetBookById(int id)
+        {
+            return _bookModel.GetBookById(id);
+        }
+
+        public List<BookModel> SearchBook(string title, string author)
+        {
+            return _bookModel.SearchBook(title, author);
+        }
+
     }
 }
---
+++ BookStore/BookStore/Models/BookModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.Models
+{
+    public class BookModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+
+        public List<BookModel> GetAllBooks()
+        {
+            return DataSource();
+        }
+
+        public BookModel GetBookById(int id)
+        {
+            return DataSource().Where(x => x.Id == id).FirstOrDefault();
+        }
+
+        public List<BookModel> SearchBook(string title, string author)
+        {
+            return DataSource().Where(x => x.Author.Contains(author) || x.Title.Contains(title)).ToList();
+        }
+
+        private List<BookModel> DataSource()
+        {
+            List<BookModel> sveKnjige = new List<BookModel>();
+
+            sveKnjige.Add(new BookModel { Id = 1, Title = "ASP.Net", Author = "John" });
+            sveKnjige.Add(new BookModel { Id = 2, Title = "Java", Author = "Monika" });
+            sveKnjige.Add(new BookModel { Id = 3, Title = "C#", Author = "Ajdin" });
+
+            return sveKnjige;
+        }
+
+    }
+}
